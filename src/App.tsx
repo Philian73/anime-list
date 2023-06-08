da4438dfd778366f743470685e2d50cd72313a79
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-08 23:28:02 +0300

    Added 2 useState (lists, animeLists) and render Tabs component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,67 @@
 import './styles/App.css'
+import { useState } from 'react'
+
+import { v1 } from 'uuid'
+
+import { Tabs } from './components/Tabs/Tabs.tsx'
+
+export type ViewType = 'Смотрю' | 'В планах' | 'Просмотрено' | 'Брошено' | 'Любимое'
+
+export type ListType = {
+  id: string
+  title: ViewType
+}
+
+export type AnimeType = {
+  id: string
+  title: string
+  image: string
+}
+
+export type AnimeListsType = {
+  [key: string]: AnimeType[]
+}
 
 const App = () => {
-  return <></>
+  const animeListId_1 = v1()
+  const animeListId_2 = v1()
+  const animeListId_3 = v1()
+  const animeListId_4 = v1()
+  const animeListId_5 = v1()
+
+  const [lists, setLists] = useState<ListType[]>([
+    { id: animeListId_1, title: 'Смотрю' },
+    { id: animeListId_2, title: 'В планах' },
+    { id: animeListId_3, title: 'Просмотрено' },
+    { id: animeListId_4, title: 'Брошено' },
+    { id: animeListId_5, title: 'Любимое' },
+  ])
+
+  const [animeLists, setAnimeLists] = useState<AnimeListsType>({
+    [animeListId_1]: [
+      {
+        id: v1(),
+        title: 'Тестовое аниме',
+        image: 'https://yummy-anime.ru/static/posters/medium/1636667140.webp',
+      },
+    ],
+    [animeListId_2]: [
+      {
+        id: v1(),
+        title: '2222',
+        image: 'https://yummy-anime.ru/static/posters/medium/1636667140.webp',
+      },
+    ],
+    [animeListId_3]: [],
+    [animeListId_4]: [],
+    [animeListId_5]: [],
+  })
+
+  return (
+    <>
+      <Tabs lists={lists} animeLists={animeLists} />
+    </>
+  )
 }
 
 export default App

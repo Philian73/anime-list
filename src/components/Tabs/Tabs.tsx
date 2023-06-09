5f1d15810a9a7af98dc8f4079d49b249f505497e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-09 14:38:00 +0400

    Divided into separate components

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -2,6 +2,8 @@ import { FC, useState } from 'react'
 
 import { AnimeListsType, ListType, ViewType } from '../../App.tsx'
 
+import { Tab } from './Tab/Tab.tsx'
+import { Table } from './Table/Table.tsx'
 import cls from './Tabs.module.css'
 
 type PropsType = {
@@ -11,40 +13,26 @@ type PropsType = {
 export const Tabs: FC<PropsType> = ({ lists, animeLists }) => {
   const [activeTab, setActiveTab] = useState<ViewType>('Смотрю')
 
-  const onChangeTab = (tab: ViewType) => {
+  const onChangeActiveTab = (tab: ViewType) => {
     setActiveTab(tab)
   }
 
   const list = lists.find(list => list.title === activeTab)
+  const animeList = list ? animeLists[list.id] : []
+
+  const tabsMap = lists.map(list => {
+    const onClick = () => onChangeActiveTab(list.title)
+
+    return (
+      <Tab key={list.id} title={list.title} active={activeTab === list.title} onClick={onClick} />
+    )
+  })
 
   return (
-    <div>
-      <div className={cls.buttonsContainer}>
-        {lists.map(list => {
-          const onClick = () => onChangeTab(list.title)
-
-          return (
-            <button
-              key={list.id}
-              onClick={onClick}
-              className={cls.button + (activeTab === list.title ? ' ' + cls.active : '')}
-            >
-              {list.title}
-            </button>
-          )
-        })}
-      </div>
-      <ul className={cls.list}>
-        {list &&
-          animeLists[list.id].map(anime => {
-            return (
-              <li key={anime.id} className={cls.item}>
-                <img src={anime.image} alt={`Постер из аниме ${anime.title}`} />
-                <span>{anime.title}</span>
-              </li>
-            )
-          })}
-      </ul>
+    <div className={cls.container}>
+      <div className={cls.tabs}>{tabsMap}</div>
+      <Table animeList={animeList} />
+      {list && !animeLists[list.id].length && <span>Список пуст</span>}
     </div>
   )
 }

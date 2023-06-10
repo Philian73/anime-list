1e08e6350c5357bafd3fae5dc3595c45ac9d473e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-06-10 20:46:09 +0400

    useState + callback addAnime

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from 'react'
+import { ChangeEvent, FC, useState } from 'react'
 
-import { AnimeListsType, ListType, ViewType } from '../../App.tsx'
+import { AnimeInputModelType, AnimeListsType, ListType, ViewType } from '../../types'
 
 import { Tab } from './Tab/Tab.tsx'
 import { Table } from './Table/Table.tsx'
@@ -9,15 +9,23 @@ import cls from './Tabs.module.css'
 type PropsType = {
   lists: ListType[]
   animeLists: AnimeListsType
+  addAnime: (listId: string, inputs: AnimeInputModelType) => void
 }
-export const Tabs: FC<PropsType> = ({ lists, animeLists }) => {
+export const Tabs: FC<PropsType> = ({ lists, animeLists, addAnime }) => {
   const [activeTab, setActiveTab] = useState<ViewType>('Смотрю')
+  const [newAnimeValues, setNewAnimeValues] = useState<AnimeInputModelType>({
+    title: '',
+    image: '',
+    type: 'TV',
+    episodes: 0,
+    rating: 0,
+  })
 
   const onChangeActiveTab = (tab: ViewType) => {
     setActiveTab(tab)
   }
 
-  const list = lists.find(list => list.title === activeTab)
+  const list = lists.find(list => list.title === activeTab) as ListType
   const animeList = list ? animeLists[list.id] : []
 
   const tabsMap = lists.map(list => {
@@ -28,11 +36,70 @@ export const Tabs: FC<PropsType> = ({ lists, animeLists }) => {
     )
   })
 
+  const onNewAnimeValuesChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const { name, value } = event.currentTarget
+
+    setNewAnimeValues(prevAnime => ({
+      ...prevAnime,
+      [name]: value,
+    }))
+  }
+
+  const addAnimeCallback = () => {
+    addAnime(list.id, newAnimeValues)
+
+    setNewAnimeValues({
+      title: '',
+      image: '',
+      type: 'TV',
+      episodes: 0,
+      rating: 0,
+    })
+  }
+
   return (
     <div className={cls.container}>
       <div className={cls.tabs}>{tabsMap}</div>
       <Table animeList={animeList} />
-      {list && !animeLists[list.id].length && <span>Список пуст</span>}
+      {!animeList.length && <span>Список пуст</span>}
+      <div className={cls.addItem}>
+        <h2>Добавить новое аниме</h2>
+        <input
+          type="text"
+          name={'title'}
+          value={newAnimeValues.title}
+          onChange={onNewAnimeValuesChange}
+        />
+
+        <input
+          type="text"
+          name={'image'}
+          value={newAnimeValues.image}
+          onChange={onNewAnimeValuesChange}
+        />
+
+        <select onChange={onNewAnimeValuesChange} name="type" value={newAnimeValues.type}>
+          <option value="TV">TV</option>
+          <option value="OVA">OVA</option>
+          <option value="film">Film</option>
+        </select>
+
+        <input
+          type="number"
+          name={'episodes'}
+          value={newAnimeValues.episodes}
+          onChange={onNewAnimeValuesChange}
+        />
+
+        <input
+          type="number"
+          name="rating"
+          value={newAnimeValues.rating}
+          onChange={onNewAnimeValuesChange}
+        />
+
+        <button onClick={addAnimeCallback}>Добавить</button>
+      </div>
     </div>
   )
 }
